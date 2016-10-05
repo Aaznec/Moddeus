d41c0af9c3503536534d01fd1be243ca0dd2abd7
Aaznec <Aaznec@tuta.io>
2016-10-05 19:25:21 +0100

    Initial commit

---
+++ core/src/com/aaznec/moddeus/Moddeus.java
@@ -0,0 +1,11 @@
+package com.aaznec.moddeus;
+
+import com.badlogic.gdx.Game;
+
+public class Moddeus extends Game {
+
+    @Override
+    public void create() {
+    }
+	
+}
---
+++ desktop/src/com/aaznec/moddeus/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aaznec.moddeus.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aaznec.moddeus.Moddeus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Moddeus(), config);
+	}
+}
